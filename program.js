58dc9c0c8815e730deb457d693b8e09e02a2a9fb
nguyenxuanquang30061999 <quang.nx0628@sinhvien.hoasen.edu.vn>
2021-02-01 12:23:28 +0700

    Code Baby

--- program.js
+++ program.js
@@ -32,9 +32,79 @@ function decToBin(num) {
     for (let i = arr.length - 1; i > -1; i--) {
         console.log(arr[i]);
     }
-
 }
-decToBin(10);
+
+function binToDec(input) {
+    let temp = [];
+    temp = input.toString().split("");
+    temp = temp.reverse();
+    let kq = 0;
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i] == 1) {
+            kq += 1 * 2 * Math.pow(2, i);
+        }
+    }
+    return kq;
+}
+
+function binToHex(n) {
+    n = n.toString();
+    for (let i = 0; i < nLength; i++) {
+        if ((n.length) % 4 != 0) {
+            n = "0" + n;
+        }
+    }
+    let temp = [];
+    for (let i = 0; i < n.length; i++) {
+        temp.push(n.substring(i, i + 4))
+    }
+    for (let i = 0; i < temp.length; i++) {
+        kq = kq + hex(temp[i]);
+    }
+    return kq;
+}
+
+function hex(n) {
+    switch (n) {
+        case 0000:
+            return '0';
+        case 0001:
+            return '1';
+        case 0010:
+            return '2';
+        case 0011:
+            return '3';
+        case 0100:
+            return '4';
+        case 0101:
+            return '5';
+        case 0110:
+            return '6';
+        case 0111:
+            return '7';
+        case 1000:
+            return '8';
+        case 1001:
+            return '9';
+        case 1010:
+            return 'A';
+        case 1011:
+            return 'B';
+        case 1100:
+            return 'C';
+        case 1101:
+            return 'D';
+        case 1110:
+            return 'E';
+        case 1111:
+            return 'F';
+        default:
+            break;
+    }
+}
+
+
+
+
 
 module.exports = main;
-module.exports = decToBin;
